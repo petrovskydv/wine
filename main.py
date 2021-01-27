bf88ed400283eafbac2bd900f15328e240df51a0
petrovskyd <petrovsky.dv@gmail.com>
2021-01-27 17:50:40 +0300

    упростил код группировки, убрал пустые значения из словарей

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pprint import pprint
-
+import collections
 
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def read_wine_file_to_dict(file_path):
-    excel_data_df = pandas.read_excel(file_path)
+    excel_data_df = pandas.read_excel(file_path, keep_default_na=False)
     wines = excel_data_df.to_dict(orient='records')
     return wines
 
@@ -22,12 +22,10 @@ def main():
 
     age = datetime.datetime.now().year - 1921
     wines = read_wine_file_to_dict('wine2.xlsx')
-    wines_categories = {}
+    wines_categories = collections.defaultdict(list)
     for wine in wines:
-        if wine['Категория'] in wines_categories:
-            wines_categories[wine['Категория']].append(wine)
-        else:
-            wines_categories[wine['Категория']] = [wine]
+        wines_categories[wine['Категория']].append(wine)
+
     pprint(wines_categories)
 
     rendered_page = template.render(age=age, wines=wines)
